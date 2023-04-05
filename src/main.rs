ea13e4ca6c436f31019995717407cc1dbe643aad
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-04-05 14:37:01 +0200

    adding a first call

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod problems;
 
 fn main() {
-    println!("Hello World");
+    println!("{}",problems::shape_func::approx_func([1.0f64,1.0,1.0,1.0,1.0,1.0], 0.5, 0.5, 0.0));
 }
 
--- src/problems.rs
+++ src/problems.rs
@@ -1,5 +1,5 @@
 
-mod shape_func {
+pub mod shape_func {
     /// Shape Function for the 1 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
     pub fn BarN1(xi1: f64, _xi2: f64, _xi3: f64) -> f64 {
@@ -29,5 +29,20 @@ mod shape_func {
     /// See theory.tex for clarificatiom
     pub fn BarN6(_xi1: f64, xi2: f64, xi3: f64) -> f64 {
         -2.0*xi2*(xi2 - 1.0)-2.0*xi3*(xi3 - 1.0)
+    }
+
+    pub fn approx_func(weights: [f64;6], xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        weights[0] * BarN1(xi1, xi2, xi3)
+        + weights[1] * BarN2(xi1, xi2, xi3)
+        + weights[2] * BarN3(xi1, xi2, xi3)
+        + weights[3] * BarN4(xi1, xi2, xi3)
+        + weights[4] * BarN5(xi1, xi2, xi3)
+        + weights[5] * BarN6(xi1, xi2, xi3)
+    }
+}
+
+pub mod phase_field {
+    pub fn phase_field_func(fbase:f64, kreg:f64, l: f64) -> f64 {
+        (- fbase / (fbase.powi(2) + kreg).powf(0.25) * 1.0/l).exp()
     }
 }
