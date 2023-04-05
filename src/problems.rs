2a67c848d9e0dc8738f98b605ec650d8206cc703
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-04-05 14:02:31 +0200

    adding documentation

--- src/problems.rs
+++ src/problems.rs
@@ -5,18 +5,28 @@ mod shape_func {
     pub fn BarN1(xi1: f64, _xi2: f64, _xi3: f64) -> f64 {
         2.0*xi1*(xi1 - 0.5)
     }
+    /// Shape Function for the 2 Node of a 6-Node Triangular Element
+    /// See theory.tex for clarificatiom
     pub fn BarN2(_xi1: f64, xi2: f64, _xi3: f64) -> f64 {
         2.0*xi2*(xi2 - 0.5)
     }
+    /// Shape Function for the 3 Node of a 6-Node Triangular Element
+    /// See theory.tex for clarificatiom
     pub fn BarN3(_xi1: f64, _xi2: f64, xi3: f64) -> f64 {
         2.0*xi3*(xi3 - 0.5)
     }
+    /// Shape Function for the 4 Node of a 6-Node Triangular Element
+    /// See theory.tex for clarificatiom
     pub fn BarN4(xi1: f64, _xi2: f64, xi3: f64) -> f64 {
         -2.0*xi1*(xi1 - 1.0)-2.0*xi3*(xi3 - 1.0)
     }
+    /// Shape Function for the 5 Node of a 6-Node Triangular Element
+    /// See theory.tex for clarificatiom
     pub fn BarN5(xi1: f64, xi2: f64, _xi3: f64) -> f64 {
         -2.0*xi1*(xi1 - 1.0)-2.0*xi2*(xi2 - 1.0)
     }
+    /// Shape Function for the 6 Node of a 6-Node Triangular Element
+    /// See theory.tex for clarificatiom
     pub fn BarN6(_xi1: f64, xi2: f64, xi3: f64) -> f64 {
         -2.0*xi2*(xi2 - 1.0)-2.0*xi3*(xi3 - 1.0)
     }
