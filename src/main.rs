270dc8287864550b3e9599f587761d7725b70074
Chenjox <chenjox.diepelt@gmx.de>
2023-04-05 18:55:50 +0200

    adding triangle data structures

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
+use indextree::Arena;
+use ndarray::prelude::*;
+
 mod problems;
+mod domain;
+
+type Point2D = Array2<f64>;
+type Point3D = Array3<f64>;
+
+struct Triangle {
+    p1: Point2D,
+    p2: Point2D,
+    p3: Point2D
+}
+
+impl Triangle {
+    fn get_point_barycentric(self, xi1: f64, xi2: f64, xi3: f64) -> Point2D {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
+        return xi1 * self.p1 + xi2 * self.p2 + xi3 * self.p3;
+    }
+}
+
+struct Subdivision {
+    p1: Point3D,
+    p2: Point3D,
+    p3: Point3D
+}
+
+impl Subdivision {
+    fn get_point_barycentric(self, xi1: f64, xi2: f64, xi3: f64) -> Point3D {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
+        return xi1 * self.p1 + xi2 * self.p2 + xi3 * self.p3;
+    }
+}
 
 fn main() {
     println!("{}",problems::shape_func::approx_func([1.0f64,1.0,1.0,1.0,1.0,1.0], 0.5, 0.5, 0.0));
+
+    let tree = &mut Arena::new();
+
+    let a = tree.new_node(1);
+    let b = tree.new_node(11);
+    let c = tree.new_node(12);
+    let d = tree.new_node(13);
+
+    a.append(b, tree);
+    a.append(c, tree);
+    a.append(d, tree);
+
+    
+    println!("{:?}",tree);
+
+    for node in a.descendants(tree) {
+        println!("{}",tree[node].get())
+    }
 }
 
--- src/problems.rs
+++ src/problems.rs
@@ -2,33 +2,39 @@
 pub mod shape_func {
     /// Shape Function for the 1 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN1(xi1: f64, _xi2: f64, _xi3: f64) -> f64 {
+    pub fn BarN1(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
         2.0*xi1*(xi1 - 0.5)
     }
     /// Shape Function for the 2 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN2(_xi1: f64, xi2: f64, _xi3: f64) -> f64 {
+    pub fn BarN2(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
         2.0*xi2*(xi2 - 0.5)
     }
     /// Shape Function for the 3 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN3(_xi1: f64, _xi2: f64, xi3: f64) -> f64 {
+    pub fn BarN3(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
         2.0*xi3*(xi3 - 0.5)
     }
     /// Shape Function for the 4 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN4(xi1: f64, _xi2: f64, xi3: f64) -> f64 {
-        -2.0*xi1*(xi1 - 1.0)-2.0*xi3*(xi3 - 1.0)
+    pub fn BarN4(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
+        4.0*xi1*xi3
     }
     /// Shape Function for the 5 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN5(xi1: f64, xi2: f64, _xi3: f64) -> f64 {
-        -2.0*xi1*(xi1 - 1.0)-2.0*xi2*(xi2 - 1.0)
+    pub fn BarN5(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
+        4.0*xi1*xi2
     }
     /// Shape Function for the 6 Node of a 6-Node Triangular Element
     /// See theory.tex for clarificatiom
-    pub fn BarN6(_xi1: f64, xi2: f64, xi3: f64) -> f64 {
-        -2.0*xi2*(xi2 - 1.0)-2.0*xi3*(xi3 - 1.0)
+    pub fn BarN6(xi1: f64, xi2: f64, xi3: f64) -> f64 {
+        if xi1 + xi2 + xi3 > 1.0 {panic!("Illegal Barycentric Coordinates, {},{},{} = {} > 1.0!",xi1,xi2,xi3,xi1+xi2+xi3);};
+        4.0*xi2*xi3
     }
 
     pub fn approx_func(weights: [f64;6], xi1: f64, xi2: f64, xi3: f64) -> f64 {
