cdb0df21d015cc361dbf13cb44918dc36e8ca2a2
Chenjox <chenjox.diepelt@gmx.de>
2023-08-17 15:45:28 +0200

    adding edge subdivision child transformation

--- src/integration_2d/integrators/edge_subdivision_integrator.rs
+++ src/integration_2d/integrators/edge_subdivision_integrator.rs
@@ -42,12 +42,13 @@ impl<I: Simplex2DIntegrator<IntegratorDummy>> Simplex2DIntegrator<IntegratorDumm
                 // 0,-1,1
                 let i0 = i0 +1;
                 if i1 != 0 {
-                  let transformation = array![
+                  let ch_transformation = array![
                     [i0 as f64 / order_fl,     i1 as f64/order_fl, i2 as f64 / order_fl],
                     [(i0-1) as f64 / order_fl, i1 as f64/order_fl, (i2+1) as f64 / order_fl],
                     [i0 as f64 / order_fl,     (i1-1) as f64/order_fl, (i2+1) as f64 / order_fl],
                   ];
-                  let transformation = transformation.reversed_axes();
+                  let ch_transformation = ch_transformation.reversed_axes();
+                  let transformation = transformation.dot(&ch_transformation);
                   result += self.base_integrator.integrate_over_domain(&transformation, func, simplex, &mut IntegratorDummy);
                   //println!("{},{},{}", i0,i1,i2);
                   //println!("{},{},{}", i0,i1-1,i2+1);
@@ -55,12 +56,13 @@ impl<I: Simplex2DIntegrator<IntegratorDummy>> Simplex2DIntegrator<IntegratorDumm
                   //println!("{},{},{}", i0-1,i1,i2+1);
                 }
                 // -1,1,0
-                let transformation = array![
+                let ch_transformation = array![
                   [i0 as f64 / order_fl,     i1 as f64/order_fl, i2 as f64 / order_fl],
                   [(i0-1) as f64 / order_fl, (i1+1) as f64/order_fl, (i2) as f64 / order_fl],
                   [(i0-1) as f64 / order_fl, i1 as f64/order_fl,     (i2+1) as f64 / order_fl],
                 ];
-                let transformation = transformation.reversed_axes();
+                let ch_transformation = ch_transformation.reversed_axes();
+                let transformation = transformation.dot(&ch_transformation);
                 result += self.base_integrator.integrate_over_domain(&transformation, func, simplex, &mut IntegratorDummy);
                 //println!("{},{},{}", i0,i1,i2);
                 //println!("{},{},{}", i0-1,i1+1,i2+0);
